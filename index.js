ebd499b96b4d0d65ff8cc1a586570daff41e0881
Irham F Rufdo <irham.f.rufdo@gmail.com>
2022-08-29 13:53:48 +0700

    update My Project

--- index.js
+++ index.js
@@ -1,182 +1,317 @@
 const express = require ('express')
+const bcrypt = require('bcrypt')
+const flash = require('express-flash')
+const session = require('express-session')
 
 const db = require('./dbconnect/db')
 
 const app = express ()
 const port = 5600
 
-app.set ('view engine', 'hbs')
+app.set('view engine', 'hbs')
 app.use('/assets', express.static(__dirname + '/assets'))
+
+app.use(flash())
+
+app.use(
+    session({
+        cookie: {
+            httpOnly: true,
+            secure: false,
+            maxAge: 1000 * 60 * 60 * 2
+        },
+        store: new session.MemoryStore(),
+        saveUninitialized: true,
+        resave: false,
+        secret: 'secretValue'
+    })
+)
+
 app.use(express.urlencoded({extended: false}))
 
 let isLogin = true
 
-// let dataProject = [
-//     {
-//         title: 'Programming Nowadays',
-//         content: 'Computer programming is the process of performing a particular computation (or more generally, accomplishing a specific computing result), usually by designing and building an executable computer program. Programming involves tasks such as analysis, generating algorithms, profiling algorithms accuracy and resource consumption, and the implementation of algorithms (usually in a chosen programming language, commonly referred to as coding). The source code of a program is written in one or more languages that are intelligible to programmers, rather than machine code, which is directly executed by the central processing unit. The purpose of programming is to find a sequence of instructions that will automate the performance of a task (which can be as complex as an operating system) on a computer, often for solving a given problem. Proficient programming thus usually requires expertise in several different subjects, including knowledge of the application domain, specialized algorithms, and formal logic.',
-//         startDate: '11 Juli 2022',
-//         endDate: '18 Juli 2022',
-//         duration: '1',
-//         author: 'Irham Fatriyand',
-//         checkedValue: '',
-//         image: ''
-//     }
-// ]
-
 app.get ('/', function (request, response) {
-
-    let selectQuery = 'SELECT * FROM tb_projects;'
 
     db.connect((err,client,done)=>{
         if(err) {
             return response.redirect('/contact-me')
         }
 
+        let selectQuery = 'SELECT * FROM tb_projects;'
+
         client.query(selectQuery, (err,result) =>{
             if(err) throw err
 
-            let dataPro = result.rows
+            let dataProject = result.rows
 
-            dataPro = dataPro.map((project) =>{
+            dataProject = dataProject.map((item) =>{
                 return {
-                    ...project,
+                    ...item,
                     isLogin,
                     author: 'Irham Fatriyand',
-                    duration: '1 month'
+                    duration: getDuration(new Date())
                 }
             })
 
-            response.render ('index' , {isLogin, dataProject: dataPro})
+            response.render ('index' , {dataProject, isLogin: request.session.isLogin, user: request.session.user})
+        })
+    })
+
+}) 
+
+app.get ('/project-detail/:id', function (request, response) {
+    let id = request.params.id
+
+    
+    db.connect((err,client,done)=>{
+        if(err) {
+            return response.redirect('/contact-me')
+        }
+
+        let detailQuery = `SELECT * FROM tb_projects WHERE id=${id}`
+
+        client.query(detailQuery, (err,result) =>{
+            if(err) throw err
+
+            let data = result.rows
+
+            let dataProject = data.map((item) =>{
+                return {
+                    ...item,
+                    isLogin,
+                    author: 'Irham Fatriyand',
+                    start_date: getFullTime(item.start_date),
+                    end_date: getFullTime(item.end_date),
+                    duration: getDuration()
+                }
+            })
+
+            response.render ('project-detail', {isLogin: request.session.isLogin, user: request.session.user, data:dataProject[0]})
+        })
+    })
+
+})
+
+app.get ('/project', function (request, response) {
+
+    if (!isLogin) {
+        return res.redirect('/')
+    }
+    response.render ('project')
+})
+
+app.post ('/project', function(request, response) {
+
+    let { inputTitle: name, startDate: start_date, endDate: end_date, inputContent: description, checkedValue: technologies, imageContent: image } = request.body
+
+    db.connect((err,client,done)=>{
+        if(err) {
+            return response.redirect('/contact-me')
+        }
+
+        let postQuery = `INSERT INTO tb_projects (name, start_date, end_date, description, image) 
+                            VALUES ('${name}','${start_date}','${end_date}','${description}','${image}')`
+
+        client.query(postQuery, (err,result) =>{
+            done()
+            if(err) throw err
+
+            response.redirect ('/')
+        })
+    })
+})
+
+app.get ('/update/:id', function (request, response) {
+    let id = request.params.id
+
+    db.connect((err,client,done)=>{
+        if(err) {
+            return response.redirect('/contact-me')
+        }
+
+        let delQuery = `SELECT * FROM tb_projects WHERE id=${id}`
+
+        client.query(delQuery, (err,result) =>{
+            if(err) throw err
+
+            let data = result.rows
+
+            let dataProject = data.map((item) =>{
+                return {
+                    ...item,
+                    isLogin,
+                    author: 'Irham Fatriyand',
+                    duration: getDuration()
+                }
+            })
+
+            response.render ('update', {data:dataProject[0]})
+        })
+    })
+    
+})
+
+app.post('/update/:id', function(request, response){
+    
+    let { id, inputTitle: name, startDate: start_date, endDate: end_date, inputContent: description, checkedValue: technologies, imageContent: image } = request.body
+    
+    db.connect((err,client,done)=>{
+        if(err) {
+            return response.redirect('/contact-me')
+        }
+
+        let updateQuery = `UPDATE tb_projects
+        SET name='${name}', start_date='${start_date}', end_date='${end_date}', description='${description}', image='${image}'
+        WHERE id=${id}`
+
+        client.query(updateQuery, (err,result) =>{
+            if(err) throw err
+        
+            response.redirect ('/')
+        })
+    
+    })
+})
+
+app.get('/delete/:id', function(request, response) {
+
+    let id = request.params.id
+    let delQuery = `DELETE FROM tb_projects WHERE id=${id}`
+
+    db.connect((err,client,done)=>{
+        if(err) {
+            return response.redirect('/contact-me')
+        }
+
+        client.query(delQuery, (err,result) =>{
+            if(err) throw err
+
+            response.redirect('/')
         })
     })
 
     
-}) 
+})
 
 app.get ('/register', function (request, response) {
     response.render ('register')
+})
+
+app.post ('/register', function (request, response) {
+    let { name, email, password } = request.body
+
+    password = bcrypt.hashSync(password, 10);
+
+    let checkQuery = `SELECT * FROM "tb_users" WHERE email='${email}'`
+
+    let query = `INSERT INTO tb_users (name, email, password) 
+                VALUES  ('${name}', '${email}', '${password}')`
+
+    db.connect ((err, client, done)=> {
+        if(err) {
+            return response.redirect('/contact-me')
+        }
+
+        client.query(checkQuery, (err, result) => {
+            if (err) throw err
+
+            if (result.rowCount != 0) {
+                return response.redirect('/register')
+            }
+
+            client.query(query, (err,result) =>{
+                done()
+                if(err) throw err
+    
+                response.redirect('/login')
+            })
+        })
+
+    })
+
 })
 
 app.get ('/login', function (request, response) {
     response.render ('login')
 })
 
+app.post('/login', function (request, response) {
+    let { email, password } = request.body
+
+    db.connect((err, client, done) => {
+        if (err) throw err
+
+        let checkQuery = `SELECT * FROM "tb_users" WHERE email='${email}'`
+
+        client.query(checkQuery, (err, result) => {
+            if (err) throw err
+
+            if (result.rowCount == 0) {
+                request.flash('danger', 'email not found')
+
+                return response.redirect('/login')
+            }
+
+            let isMatch = bcrypt.compareSync(password, result.rows[0].password)
+
+            if (isMatch) {
+                request.session.isLogin = true
+                request.session.user = {
+                    name: result.rows[0].name,
+                }
+                request.flash('success', 'login success')
+                response.redirect('/')
+            } else {
+                request.flash('danger', 'email and password doesnt match')
+
+                response.redirect('/login')
+            }
+
+        })
+    })
+})
+
+app.get('/logout', function (request, response) {
+    request.session.destroy()
+    response.redirect('/')
+})
+
 app.get ('/contact-me', function (request, response) {
     response.render ('contact-me')
 })
 
-app.get ('/project', function (request, response) {
-    response.render ('project')
-})
 
-app.post ('/project', function(request, response) {
-    let title = request.body.inputTitle
-    let content = request.body.inputContent
-    let startDate = request.body.startDate
-    let endDate = request.body.endDate
-    let checkedValue = request.body.checkedValue
-    // let duration = request.body.duration
-    let image = request.body.image
 
-    start = new Date(startDate)
-    end = new Date(endDate)
+function getFullTime(time) {
 
-    startDated = start.getMonth()
-    endDated = end.getMonth()
+    let month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-    let duration = endDated - startDated
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
 
-    let project = {
-        title,
-        content,
-        author: "Irham Fatriyand",
-        startDate,
-        endDate,
-        checkedValue,
-        duration,
-        image
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+    if (hours < 10) {
+        hours = "0" + hours
+    } else if (minutes < 10) {
+        minutes = "0" + minutes
     }
 
-    dataProject.push(project)
 
-    response.redirect('/')
-})
- 
-app.get ('/update/:id', function (request, response) {
-    let id = request.params.id
+    let fullTime = `${date} ${month[monthIndex]} ${year}`
 
-    let data = {
-        title: dataProject[id].title,
-        content: dataProject[id].content,
-        startDate: dataProject[id].startDate,
-        endDate: dataProject[id].endDate,
-        checkedValue: dataProject[id].checkedValue,
-        duration: dataProject[id].duration,
-        image: dataProject[id].image
-    }
-    response.render ('update', {id, data})
-})
+    return fullTime
+}
 
-app.post('/update/:id', function(request, response){
-
-    let id = request.params.id
-
-    dataProject[id].title = request.body.inputTitle
-    dataProject[id].content = request.body.inputContent
-    dataProject[id].startDate = request.body.startDate
-    dataProject[id].endDate = request.body.endDate
-    dataProject[id].checkedValue = request.body.checkedValue
-    dataProject[id].image = request.body.image
+function getDuration(start_date, end_date){
     
+    var timeStart = new Date(start_date)
+    var timeEnd = new Date(end_date)
 
-    start = new Date(request.body.startDate)
-    end = new Date(request.body.endDate)
-    
-    startDated = start.getMonth()
-    endDated = end.getMonth()
-    
-    let duration = endDated - startDated
-
-    dataProject[id].duration = duration
-
-    response.redirect('/')
-})
-
-app.get('/delete/:id', function(request, response) {
-
-    let id = request.params.id
-
-    dataProject.splice(id, 1)
-
-    response.redirect('/')
-})
-
-app.get ('/project-detail/:id', function (request, response) {
-    let id = request.params.id
-    console.log(id);
-
-    let data = dataProject[id]
-    data = {
-        title: data.title,
-        content: data.content,
-        duration: data.duration,
-        startDate: data.startDate,
-        endDate: data.endDate,
-        author: data.author,
-        checkedValue: data.checkedValue,
-        image: data.image
-    }
-
-    response.render ('project-detail', {data})
-})
-
-function getDuration(start, end){
-    
-    let timeStart = new Date(start)
-    let timeEnd = new Date(end)
-
-    distance = timeEnd - timeStart
+    var distance = Math.abs(timeEnd.getTime() - timeStart.getTime())
 
     let monthDistance = Math.floor(distance / (30 * 24 * 60 * 60 * 1000))
     if (monthDistance != 0) {
@@ -206,7 +341,6 @@ function getDuration(start, end){
             }
         }
     }
-    return `${finalDur}`
 }
 
 app.listen(port, function () {
